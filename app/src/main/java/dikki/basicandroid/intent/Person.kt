f93be0adc7a9a092855a10173db06f112f186400
Dikki AP <dikki.1710@gmail.com>
2023-03-18 20:08:27 +0700

    Fixing Implement Intent with Object

--- app/src/main/java/dikki/basicandroid/intent/Person.kt
+++ app/src/main/java/dikki/basicandroid/intent/Person.kt
@@ -1,37 +1,10 @@
 package dikki.basicandroid.intent
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 // Making Data Class that implement Parcelable
 // U need to override the constructor and other function from Parcelable
-data class Person(val name: String?, val age: Int?, val email: String?, val city: String?) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeValue(age)
-        parcel.writeString(email)
-        parcel.writeString(city)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Person> {
-        override fun createFromParcel(parcel: Parcel): Person {
-            return Person(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Person?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+// U can use Annotation "@Parcelize" and extends to Parcelable Class
+@Parcelize
+data class Person(val name: String?, val age: Int?, val email: String?, val city: String?) : Parcelable
